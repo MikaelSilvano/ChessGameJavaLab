ba65c1ea2c7864805b5718c0b6d1bf0d7cd001f2
Mikael Silvano <mikaelsilvano04@gmail.com>
2024-03-14 16:56:43 +0700

    Initialize

---
+++ src/test.java
@@ -0,0 +1,18 @@
+import javax.swing.UIManager;
+import javax.swing.UIManager.*;
+public class test {
+    public static void main(String[] args) {
+        try {
+            for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            // If Nimbus is not available, you can set the GUI to another look and feel.
+        }
+        new Board();
+    }
+
+}
