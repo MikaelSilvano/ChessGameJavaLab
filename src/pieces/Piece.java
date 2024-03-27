503b8dc8f4358369977188af63db5e52375d66d3
Mikael Silvano <mikaelsilvano04@gmail.com>
2024-03-27 18:59:03 +0700

    Single Player Chess (belum ada gantian tim sama kondisi menang)

---
+++ src/pieces/Piece.java
@@ -0,0 +1,49 @@
+package pieces;
+
+import main.Board3;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Piece {
+    public int col, row; //col row dari setiap pieces
+    public int xPos, yPos; //x dan y position dari pieces
+
+    public boolean isWhite;
+    public String name;
+    public int value;
+
+    public boolean isFirstMove = true;
+
+    BufferedImage sheet;
+    {
+        try {
+            sheet = ImageIO.read(ClassLoader.getSystemResourceAsStream("PiecesImages.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    protected int sheetScale = sheet.getWidth()/6;
+
+    Image sprite; //ngambil bagian2 piece
+
+    Board3 board;
+
+    public Piece(Board3 board) {
+        this.board = board;
+    }
+
+    public boolean isValidMovement(int col, int row) {
+        return true;
+    }
+
+    public boolean moveCollidesWithPiece(int col, int row) {
+        return false;
+    }
+
+    public void paint(Graphics2D g2d) {
+        g2d.drawImage(sprite, xPos, yPos, null);
+    }
+}
